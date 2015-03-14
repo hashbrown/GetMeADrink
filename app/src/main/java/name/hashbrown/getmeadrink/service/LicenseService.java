7c84c1297c1b1fcb251dd9fa057683e6e9b5ae6a
Brad Armstrong <brada@code42.com>
2015-03-14 14:30:06 -0500

    Cleanup unnecessary code in LicenseService, rename ApiResponse.

--- app/src/main/java/name/hashbrown/getmeadrink/service/LicenseService.java
+++ app/src/main/java/name/hashbrown/getmeadrink/service/LicenseService.java
@@ -1,23 +1,10 @@
 package name.hashbrown.getmeadrink.service;
 
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.TypeAdapter;
-import com.google.gson.TypeAdapterFactory;
-import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
-
-import java.io.IOException;
-
-import name.hashbrown.getmeadrink.model.ApiResponse;
+import name.hashbrown.getmeadrink.model.LicenseResponse;
 import name.hashbrown.getmeadrink.model.Feature;
 import name.hashbrown.getmeadrink.model.License;
 import retrofit.RestAdapter;
-import retrofit.converter.GsonConverter;
 import rx.Observable;
 import rx.functions.Func1;
 
@@ -29,13 +16,8 @@ public class LicenseService {
 
     public LicenseService(){
 
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapterFactory(new ItemTypeAdapterFactory())
-                .create();
-
         RestAdapter restAdapter = new RestAdapter.Builder()
                 .setEndpoint(WEB_SERVICE_BASE_URL)
-//                .setConverter(new GsonConverter(gson))
                 .setLogLevel(RestAdapter.LogLevel.BASIC)
                 .build();
 
@@ -44,44 +26,18 @@ public class LicenseService {
 
     public Observable<License> getOnSalesLiquorLicenses(){
         return dataService.getOnSaleLiquorLicenses()
-                .flatMap(new Func1<ApiResponse, Observable<Feature>>() {
+                .flatMap(new Func1<LicenseResponse, Observable<Feature>>() {
                     @Override
-                    public Observable<Feature> call(ApiResponse apiResponse) {
-                        ApiResponse resp = apiResponse;
+                    public Observable<Feature> call(LicenseResponse apiResponse) {
                         return Observable.from(apiResponse.features);
                     }
                 })
                 .map(new Func1<Feature, License>() {
                     @Override
                     public License call(Feature feature) {
-                        Feature f = feature;
                         return feature.license;
                     }
                 });
-    }
-
-
-    public static class ItemTypeAdapterFactory implements TypeAdapterFactory {
-
-        public <T> TypeAdapter<T> create(Gson gson, final TypeToken<T> type) {
-
-            final TypeAdapter<T> delegate = gson.getDelegateAdapter(this, type);
-            final TypeAdapter<JsonElement> elementAdapter = gson.getAdapter(JsonElement.class);
-
-            return new TypeAdapter<T>() {
-
-                public void write(JsonWriter out, T value) throws IOException {
-                    delegate.write(out, value);
-                }
-
-                public T read(JsonReader in) throws IOException {
-
-                    JsonElement jsonElement = elementAdapter.read(in);
-
-                    return delegate.fromJsonTree(jsonElement.getAsJsonObject().get("features"));
-                }
-            }.nullSafe();
-        }
     }
 
 }
--- app/src/main/java/name/hashbrown/getmeadrink/service/MPLSOpenDataService.java
+++ app/src/main/java/name/hashbrown/getmeadrink/service/MPLSOpenDataService.java
@@ -1,11 +1,11 @@
 package name.hashbrown.getmeadrink.service;
 
-import name.hashbrown.getmeadrink.model.ApiResponse;
+import name.hashbrown.getmeadrink.model.LicenseResponse;
 import retrofit.http.GET;
 import rx.Observable;
 
 public interface MPLSOpenDataService {
 
     @GET("/On-Sale_Liquor_Licenses/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&maxAllowableOffset=0.29858214164732144&outFields=*&outSR=3857&resultOffset=0&resultRecordCount=2000")
-    Observable<ApiResponse> getOnSaleLiquorLicenses();
+    Observable<LicenseResponse> getOnSaleLiquorLicenses();
 }
